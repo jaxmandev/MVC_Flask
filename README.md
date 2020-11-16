faa682f87087513668de3ce7d1f6cce1ff482e2b
Jax <main@Jaxs-MacBook-Pro.local>
2020-11-16 13:43:53 +0000

    README.md added

---
+++ README.md
@@ -0,0 +1,21 @@
+## MVC with Flask
+
+### MVC = "Model View Controller"
+
+Display data on a web browser using HTML, CSS, JavaScript and Bootstrap
+
+Building our API
+
+Display data from Flask to a specific API call/URL/endpoint
+Why Flask for our framework?
+
+Flask is a mini web app frame work but very powerful
+Allows us to interact with a DB
+It can be used to create an API
+Allows us to integrate with HTMl, CSS and JS
+Allows us to map HTTP requests to Python e.g. URL and HTTP GET
+Allows us to set the API path as URL to view in the browser
+Example
+
+Install Flask to Python environment using pip install Flask
+To run the flask app? flask run
---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, jsonify, url_for
+
+# create an instance of an app
+app = Flask(__name__)
+
+students = [
+    {"id": 0, "title":"Mr.", "firstname":"Jax", "lastname":"Dev", "course":"DevOps"}
+    ]
+
+# use the app.route decorator
+# the localhost:5000 is the default port for Flask
+# this function runs when the URL is "/"
+@app.route("/")
+def home():
+    return "<h1> Vive la France! </h1>"
+# This function runs when the API/URL is accessed
+
+# creating our own API to display data for a specific URL
+# this is the URL to navigate to in the browser for this specific view function 
+# http://127.0.0.1:5000/api/v1/student/data
+@app.route("/api/v1/student/data", methods=["GET"]) 
+def greeting():
+    return jsonify(students)  
+    # Using ETL: Extract Transform Load
+
+# find out the module to redirect the user back to specific page (welcome page
+# if page not founds (status code 404, redirect the use to welcome page
+
+# module to redirect user back to specific page
+@app.route("/redirectme/")
+def redirect_me():
+    return redirect(url_for(greeting))
+
+
+# if any error occurs then redirects to error message page
+@app.errorhandler(Exception)
+def error_occured(error):
+    return redirect(url_for(error_com))
+
+@app.route("/error/")
+def error_com():
+    return "An error occurred"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
